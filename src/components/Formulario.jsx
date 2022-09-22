ebbd9a0896320614191869a61ae2e1cd408aa3ee
Gonzalopaz41 <gonzalopazok@gmail.com>
2022-09-22 16:51:28 -0300

    finished project

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import styled from '@emotion/styled'
+import Error from './Error'
 import useSelectMonedas from '../hooks/useSelectMonedas'
 import { monedas } from '../data/monedas'
+
 const InputSubmit = styled.input`
     background-color: #9497FF;
     border: none;
@@ -20,17 +22,56 @@ const InputSubmit = styled.input`
       background-color: #7A7DFE;
     }
 `
-const Formulario = () => {
+const Formulario = ({setMonedas}) => {
+  const [criptos, setCriptos] = useState([])
+  const [error, setError] = useState(false)
   
-  const [SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas)
+  const [moneda, SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas)
+  const [criptomoneda, SelectCriptomoneda] = useSelectMonedas('Elige tu Criptomoneda', criptos)
   
+  useEffect(() => {
+    const consultarAPI = async () => {
+        const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        const arrayCriptos = resultado.Data.map( cripto => {
+            const objeto = {
+                id: cripto.CoinInfo.Name,
+                nombre: cripto.CoinInfo.FullName
+            }
+            return objeto
+        })
+
+        setCriptos(arrayCriptos)
+
+    }
+    consultarAPI();
+}, [])
  
+const handleSubmit = e => {
+  e.preventDefault()
+   if ([moneda, criptomoneda].includes('')){
+    setError(true)
+    return
+   }
+   setError(false)
+   setMonedas({
+    moneda,
+    criptomoneda
+   })
+}
+
  return (
-    <form>
+    <>
+    {error && <Error>Todos los campos son obligatorios</Error>}
+    <form onSubmit={handleSubmit}>
         <SelectMonedas />
-        
+        <SelectCriptomoneda />
+       
         <InputSubmit type="submit" value="Cotizar" />
     </form>
+    </>
   )
 }
 
--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import {useState} from 'react'
 import styled from '@emotion/styled'
+
 const Label = styled.label`
     color: #fff;
     display: block;
@@ -17,10 +18,13 @@ const Select = styled.select`
 `
 
 const useSelectMonedas = (label, opciones) => {
- const SelectMonedas = () => (
+
+    const [state, setState] = useState('')
+
+    const SelectMonedas = () => (
     <>
         <Label>{label}</Label>
-        <Select>
+        <Select value={state} onChange={e => setState(e.target.value)}>
             <option value="">Seleccione</option>
             
             {opciones.map(opcion => (
@@ -35,7 +39,7 @@ const useSelectMonedas = (label, opciones) => {
            
     </>
  )
- return [SelectMonedas]
+ return [state, SelectMonedas]
 }
 
 export default useSelectMonedas
